5e3d93dd2907b891437584964a7aba742fed040a
chaozhan <chao930@gmail.com>
2019-12-20 15:44:17 +0800

    version1

--- data_v1.py
+++ data_v1.py
@@ -14,3 +14,15 @@ df = ts.pro_bar(ts_code='000001.SH', asset='I', start_date='20100701', end_date=
 
 df['trade_date'] = pd.to_datetime(df['trade_date'])
 df.set_index('trade_date', inplace=True)
+
+
+def create_dataset(dataset, look_back=7, foresight=3):
+    X, Y = [], []
+    for i in range(len(dataset)-look_back-foresight):
+        obs = dataset[i:(i+look_back), 0] # Sequence of 7 stock prices as features forming an observation
+       # Append sequence
+        X.append(obs)
+       # Append stock price value occurring 4 time-steps into future
+        Y.append(dataset[i + (look_back+foresight), 0])
+    return np.array(X), np.array(Y)
+
